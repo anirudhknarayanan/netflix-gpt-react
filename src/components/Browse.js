e3435284251fd4a70d989bc614e1210803f7694a
anirudh <anirudhknarayanan611@gmail.com>
2026-06-17 14:57:03 +0530

    fetch data from tmdb make an api call for now playing movies list

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './Header'
+import { API_OPTIONS } from '../utils/constants'
 
 const Browse = () => {
+  const getnowPlayingMovies = async()=>{
+    let data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS)
+    let json = await data.json()
+    console.log(json);
+    
+  }
+
+  useEffect(()=>{
+    getnowPlayingMovies()
+
+  },[])
   return (
     <div>
     <Header/>
---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+export const API_OPTIONS =  {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MjQ5ZDIwOGY0NjQ2NGQ0YmNlODY2NDg1ZDI3MzBhOCIsIm5iZiI6MTc4MTYyMzg1NS4xOTgwMDAyLCJzdWIiOiI2YTMxNmMyZjVmYTczMWVmZmNkODNjNmEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.g2WjkBs5tmt2L7trc-K_eAjara0cS7QqgsYLA3O2gtQ'
+  }
+};
